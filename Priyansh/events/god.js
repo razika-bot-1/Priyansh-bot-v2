db674bc463652b16a706956fb90eaf16eb40768b
razika-bot-1 <dimpalsingh10079@gmail.com>
2025-03-01 16:52:23 +0530

    Update god.js

--- Priyansh/events/god.js
+++ Priyansh/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100081181726230";
+  var god = "61564185188564";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
